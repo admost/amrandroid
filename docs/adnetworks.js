f655f1baef87ed6b095abe4e637611a0d8d1c7f6
Mustafa Küçükşengün <mustafa.kucuksengun@kokteyl.com>
2017-07-04 10:15:03 +0300

    creation of page

---
+++ docs/adnetworks.js
@@ -0,0 +1,209 @@
+var obj = JSON.parse('{"ad_networks":[{"name":"AMR","status":true,"app_gradle":{"dependencies":[{"package":"com.android.volley:volley:1.0.0"},{"package":"com.admost.sdk:amr:1.3.3"}]},"project_gradle":{"dependencies":[{"maven":"http://repo.admost.com:8081/artifactory/amr"}]}},{"name":"Adcolony","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:adcolony:3.1.2"},{"package":"com.android.support:support-annotations:+"}]}},{"name":"Admob & AdX","status":false,"app_gradle":{"dependencies":[{"package":"com.google.android.gms:play-services-ads:10.2.1"}]}},{"name":"AdTrial","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:adtrial:2017-05-02"},{"package":"com.google.android.gms:play-services-ads:10.2.1"}]}},{"name":"Amazon","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:amazon:5.8.1.1"}]}},{"name":"Applovin","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:applovin:7.2.0"}]},"android_manifest":{"application":[{"meta_data":{"name":"applovin.sdk.","value":"YOUR_SDK_KEY_FROM_APPLOVIN"}}]}},{"name":"Appnext","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:appnext:2.0.3.459"},{"package":"com.google.android.gms:play-services-location:10.2.1"},{"package":"com.google.android.gms:play-services-basement:10.2.1"}]}},{"name":"Avocarrot","status":false,"app_gradle":{"dependencies":[{"package":"com.android.support:recyclerview-v7:+"},{"package":"com.android.support:appcompat-v7:+"},{"package":"com.admost.sdk:avocarrot:3.8.2"},{"package":"com.google.android.exoplayer:exoplayer:r2.2.0"}]}},{"name":"Chartboost","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:chartboost:6.6.3"}]}},{"name":"Display IO","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:display-io:1.3.12"},{"package":"com.android.support:appcompat-v7:+"},{"package":"com.android.support:support-v4:+"}]}},{"name":"Facebook","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:facebook:4.23"}]}},{"name":"Flurry","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:flurry:7.0.0"}]}},{"name":"Flymob","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:flymob:2.0.5"}]}},{"name":"Fyber","status":false,"app_gradle":{"dependencies":[{"package":"com.fyber:fyber-sdk:8.11.0"}]},"project_gradle":{"dependencies":[{"maven":"https://fyber.bintray.com/maven"}]}},{"name":"Inloco","status":false,"app_gradle":{"dependencies":[{"package":"com.inlocomedia.android:android-sdk:3.0.+"},{"package":"com.android.support:support-v4:25.3.1"},{"package":"com.google.android.gms:play-services-location:10.2.1"},{"package":"com.google.android.gms:play-services-ads:10.2.1"}]}},{"name":"Inmobi","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:inmobi:6.2.3"},{"package":"com.squareup.picasso:picasso:2.5.2"},{"package":"com.android.support:appcompat-v7:+"},{"package":"com.android.support:support-v4:+"}]}},{"name":"Ironsource","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:ironsource:6.6.1"}]}},{"name":"Leadbolt","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:leadbolt:8.0"}]}},{"name":"Loopme","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:loopme:5.1.7"}]}},{"name":"Mediabrix","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:mediabrix:1.8.1"}]}},{"name":"Millenial","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:millenial:6.4.0"}]},"android_manifest":{"application":[{"provider":{"name":"com.millennialmedia.internal.utils.MediaContentProvider","authorities":"< PACKAGE_NAME >.MediaContentProvider","grantUriPermissions":"true","exported":"false"}}]}},{"name":"Mobfox","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:mobfox:3.2.7"}]}},{"name":"Mobusi","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:mobusi:3.2.0"}]}},{"name":"Mopub","status":false,"app_gradle":{"dependencies":[{"package":"com.mopub:mopub-sdk:4.15.0@aar","transitive":true}]},"project_gradle":{"dependencies":[{"jcenter":""}]}},{"name":"Nativex","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:nativex:5.5.9"}]}},{"name":"Pubnative","status":false,"app_gradle":{"dependencies":[{"package":"net.pubnative:library:2.3.16"},{"package":"net.pubnative:library.interstitial:2.3.16"},{"package":"net.pubnative:library.feed.banner:2.3.16"},{"package":"net.pubnative:library.video:2.3.16"}]}},{"name":"Revmob","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:revmob:10.0.0"}]}},{"name":"Smaato","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:smaato:6.0.0"}]}},{"name":"Startapp","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:startapp:3.6.2"}]}},{"name":"Tapjoy","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:tapjoy:11.10.2"}]}},{"name":"Tappx","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:tappx:3.0.4"}]}},{"name":"Unity","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:unity:2.1.0"}]}},{"name":"Vungle","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:vungle:4.1.0"}]}},{"name":"Youappi","status":false,"app_gradle":{"dependencies":[{"package":"com.admost.sdk:youappi:2.0.8"}]}}]}');
+
+var htmlString = "";
+
+var arrayAppGradlePackages = [];
+
+
+fillAdNetworkList(obj);
+
+var htmlProjectGradle = '<div class="col-md-11" > <p> Add the following lines inside <var> repositories{}</var> bracelets in <code>/build.gradle</code>  file </div><div class="col-md-1"><button class="btn btn-xs btn-primary" onclick="copyToClipboard(\'#code-projectgradle\')"><i class="fa fa-copy"></i></if></li> Copy</button> </div><div class="col-md-12" id="file-gradle"> <pre><code id="code-projectgradle"></code></pre> </div>';
+var htmlAppGradle = '<div class="col-md-11"> <p> Add the following lines inside <var> dependencies{}</var> bracelets in <code>/app/src/build.gradle</code>file </div><div class="col-md-1"><button class="btn btn-xs btn-primary" onclick="copyToClipboard(\'#code-appgradle\')"><i class="fa fa-copy"></i></if></li> Copy</button> </div><div class="col-md-12" id="file-appgradle"> <pre><code id="code-appgradle"></code></pre> </div>';
+var htmlAndroidManifest = '<div class="col-md-11"> <p> Add the following lines inside <var> &lt;application&gt;</var> tags in <code>/app/src/main/AndroidManifest.xml</code> file </div><div class="col-md-1"><button class="btn btn-xs btn-primary" onclick="copyToClipboard(\'#code-androidmanifest\')"><i class="fa fa-copy"></i></if></li> Copy</button> </div><div class="col-md-12" id="file-androidmanifest"> <pre><code id="code-androidmanifest"></code></pre> </div>';
+
+fillProjectGradleCode();
+fillAppGradleCode();
+fillAndroidManifestCode();
+
+
+
+function toggleAdNetworkStatus(adNetworkName) {
+    var i = getPositionOfAdNetworkOnJSONArray(adNetworkName);
+    if (obj.ad_networks[i].status == true) {
+        removeAdNetworkFromCart(i);
+    } else {
+        addAdNetworkToCart(i);
+    }
+}
+
+function addAdNetworkToCart(i) {
+    obj.ad_networks[i].status = true;
+    fillAdNetworkList(obj);
+    //fillAdNetworkCart(obj);
+    fillFileList();
+    fillProjectGradleCode();
+    fillAppGradleCode();
+    fillAndroidManifestCode();
+}
+
+function removeAdNetworkFromCart(i) {
+    obj.ad_networks[i].status = false;
+    fillAdNetworkList(obj);
+    //fillAdNetworkCart(obj);
+    fillFileList();
+    fillProjectGradleCode();
+    fillAppGradleCode();
+    fillAndroidManifestCode();
+}
+
+function getPositionOfAdNetworkOnJSONArray(adNetworkName) {
+    for (var i = 0; i < obj.ad_networks.length; i++) {
+        if (obj.ad_networks[i].name === adNetworkName) {
+            return i;
+        }
+    }
+    return null;
+}
+
+function fillAdNetworkList() {
+    htmlString = '<div class="btn-group" style="margin:8px;" id="btn-group-network-AMR"> <button type="button" id="btn-name-network-AMR" class="btn btn-success">AMR</button>  </div>&nbsp;';
+    for (var i = 1; i < obj.ad_networks.length; i++) {
+        htmlString = htmlString + '<div class="btn-group" style="margin:8px; id="btn-group-network-' + obj.ad_networks[i].name + '"> <button type="button" id="btn-name-network-' + obj.ad_networks[i].name + '" class="btn btn-default">' + obj.ad_networks[i].name + '</button> <button type="button" id="btn-icon-network-' + obj.ad_networks[i].name + '" onclick="toggleAdNetworkStatus(\'' + obj.ad_networks[i].name + '\');" class="btn btn-';
+
+        obj.ad_networks[i].status ? htmlString = htmlString + "danger" : htmlString = htmlString + "success";
+        htmlString = htmlString + ' dropdown-toggle"> <span class="fa fa-';
+
+        obj.ad_networks[i].status ? htmlString = htmlString + "minus" : htmlString = htmlString + "plus";
+        htmlString = htmlString + '"></span> </button> </div>'
+    }
+    $("#adnetwork-button-list").html(htmlString);
+}
+
+/*
+function fillAdNetworkCart() {
+    htmlString = "";
+    $('#table-network-cart > tbody:last-child').html("");
+    for (var i = 0; i < obj.ad_networks.length; i++) {
+        if (obj.ad_networks[i].status == true) {
+            $('#table-network-cart > tbody:last-child').append('<tr><td>' + obj.ad_networks[i].name + '</td></tr>');
+        }
+    }
+}
+*/
+
+function fillFileList() {
+    $('#list-of-files').html("<li><code>project.gradle</code></li><li><code>app.gradle</code></li>");
+    if (isAndroidManifestNeeded()) {
+        $('#list-of-files').append('<li><code>AndroidManifest.xml</code></li>');
+    }
+}
+
+function isAndroidManifestNeeded() {
+    for (var i = 0; i < obj.ad_networks.length; i++) {
+        if (obj.ad_networks[i].status == true) {
+            if (obj.ad_networks[i].android_manifest) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getAppGradleFileString() {
+
+    for (var i = 0; i < obj.ad_networks.length; i++) {
+        if (obj.ad_networks[i].status == true) {
+            if (obj.ad_networks[i].android_manifest) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function fillProjectGradleCode() {
+
+    $('#file-projectgradle').html(htmlProjectGradle);
+    $('#code-projectgradle').html("");
+    for (var i = 0; i < obj.ad_networks.length; i++) {
+        if (obj.ad_networks[i].project_gradle && obj.ad_networks[i].status == true) {
+            for (var j = 0; j < obj.ad_networks[i].project_gradle.dependencies.length; j++) {
+                if(obj.ad_networks[i].project_gradle.dependencies[j].maven){
+                    $('#code-projectgradle').append("maven { url '" + obj.ad_networks[i].project_gradle.dependencies[j].maven + "' }<br>");
+                }else if(obj.ad_networks[i].project_gradle.dependencies[j].jcenter != null){
+                    $('#code-projectgradle').append("jcenter (" + obj.ad_networks[i].project_gradle.dependencies[j].jcenter + ")");
+
+                }
+            }
+        }
+    }
+}
+
+function fillAppGradleCode() {
+
+    arrayAppGradlePackages = [];
+    $('#file-appgradle').html(htmlAppGradle);
+    $('#code-appgradle').html("");
+    for (var i = 0; i < obj.ad_networks.length; i++) {
+        if (obj.ad_networks[i].app_gradle && obj.ad_networks[i].status == true) {
+            for (var j = 0; j < obj.ad_networks[i].app_gradle.dependencies.length; j++) {
+                var isDuplicate = false;
+                for (var k = 0; k < arrayAppGradlePackages.length; k++) {
+                    if (arrayAppGradlePackages[k].localeCompare("compile '"+ obj.ad_networks[i].app_gradle.dependencies[j].package + "' <br>") == 0 ) {
+                        isDuplicate = true;
+                    }
+                }
+                if (isDuplicate == false) {
+                    if(obj.ad_networks[i].app_gradle.dependencies[j].transitive == true){
+                        arrayAppGradlePackages.push("compile('"+ obj.ad_networks[i].app_gradle.dependencies[j].package + "') {<br>transitive = true<br>}<br>");
+                    }else{
+                        arrayAppGradlePackages.push("compile '"+ obj.ad_networks[i].app_gradle.dependencies[j].package + "' <br>");
+                    }
+                }
+            }
+        }
+    }
+
+
+    console.log(arrayAppGradlePackages);
+
+    for (var i = 0; i < arrayAppGradlePackages.length; i++) {
+
+        $('#code-appgradle').append(arrayAppGradlePackages[i]);
+    }
+}
+
+function fillAndroidManifestCode() {
+
+    if (isAndroidManifestNeeded()) {
+        $('#file-androidmanifest').html(htmlAndroidManifest);
+        $('#code-androidmanifest').html("");
+        for (var i = 0; i < obj.ad_networks.length; i++) {
+            if (obj.ad_networks[i].android_manifest && obj.ad_networks[i].status == true) {
+
+                for (var j = 0; j < obj.ad_networks[i].android_manifest.application.length; j++) {
+                    if (obj.ad_networks[i].android_manifest.application[j].meta_data) {
+                        $('#code-androidmanifest').append("&lt;meta-data <br>android:name=\"" + obj.ad_networks[i].android_manifest.application[j].meta_data.name + "\" android:value=\"" + obj.ad_networks[i].android_manifest.application[j].meta_data.value + "\"/&gt;<br>");
+                    } else if (obj.ad_networks[i].android_manifest.application[j].provider) {
+                        $('#code-androidmanifest').append("&lt;provider <br>android:name=\""
+                            + obj.ad_networks[i].android_manifest.application[j].provider.name
+                            + "\" <br>android:authorities=\""
+                            + obj.ad_networks[i].android_manifest.application[j].provider.authorities
+                            + "\" <br>android:grantUriPermissions=\""
+                            + obj.ad_networks[i].android_manifest.application[j].provider.grantUriPermissions
+                            + "\" <br>android:exported=\""
+                            + obj.ad_networks[i].android_manifest.application[j].provider.exported
+                            + "\"/&gt;<br>");
+                    }
+
+
+                }
+            }
+        }
+
+    } else {
+        $('#file-androidmanifest').html("");
+        $('#code-androidmanifest').html("");
+    }
+
+
+}
+
+
+function getProjectGradleFileString() {
+
+}
+
+function getAndroidManifestFileString() {
+
+}
