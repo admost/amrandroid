80459db6594e99a26a5d1fec3c8a221fa41fdb21
hanilozmen <hanilozmen@gmail.com>
2020-04-10 15:14:03 +0300

    version updated to 2.0.9

--- docs/android_data.js
+++ docs/android_data.js
@@ -10,7 +10,7 @@ function getAndroidNetworks() {
                     "package":"com.android.volley:volley:1.1.0"
                  },
                  {
-                    "package":"com.admost.sdk:amr:2.0.8"
+                    "package":"com.admost.sdk:amr:2.0.9"
                  },
                  {
                     "package":"com.admost.sdk:admost:0.0.6.a16"
